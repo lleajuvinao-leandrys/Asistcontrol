de68ebc34e5c5c7c15f097331d2c02fc47729bf2
lleajuvinao-leandrys <lleajuvinao@gmail.com>
2026-09-17 17:34:22 -0500

    Add files via upload

---
+++ src/app/index.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}> hello, world!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#11c1c1",
+  },
+
+  text: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
